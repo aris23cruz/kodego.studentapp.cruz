8e8f2a6e1deec2d7e2e76b9e48fd6e76e86f0664
aris cruz <ariscruz.va@gmail.com>
2022-10-16 16:29:20 +0800

    login functions and homescreen

--- app/src/main/java/com/kodego/activity/one/studentassistanceapp/MainActivity.kt
+++ app/src/main/java/com/kodego/activity/one/studentassistanceapp/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.kodego.activity.one.studentassistanceapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.kodego.activity.one.studentassistanceapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //login
+        binding.btnLogin.setOnClickListener(){
+            var userName : String = binding.etUserName.text.toString()
+            var password : String = binding.etPassword.text.toString()
+            checkCredential(userName,password)
+        }
+    }
+
+   private fun checkCredential(userName: String, password: String): Boolean{
+        val correctUserName : String = "admin"
+        val correctPassword : String = "admin123"
+
+        if((correctUserName == userName)&&(correctPassword == password)){
+            val intent = Intent(this,HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+            Toast.makeText(applicationContext,"Logged in", Toast.LENGTH_SHORT).show()
+            return true
+        }else{
+            Toast.makeText(applicationContext,"Input correct username/password", Toast.LENGTH_SHORT).show()
+            return false
+        }
     }
 }
